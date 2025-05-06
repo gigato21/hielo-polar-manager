e977e74a47eb1dd1495c73529941a80cea4440bb
gigato21 <gatobrigantia@gmail.com>
2025-05-06 11:38:26 -0600

    Se agregó Qery

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { useQueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Dashboard from './pages/Dashboard'; 
