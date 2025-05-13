2dfb9c0b6ed020a05bdec50e6d7450410c7e67e3
gigato21 <gatobrigantia@gmail.com>
2025-05-13 21:33:51 +0000

    Ok

--- src/hooks/useEstadisticas.ts
+++ src/hooks/useEstadisticas.ts
@@ -1,6 +1,5 @@
-
 import { useState, useEffect } from 'react'
-import { supabase } from '@/integrations/supabase/client'
+import { supabase } from '@/lib/supabaseClient'
 
 type EstadisticasProps = {
   dateRange?: { from: Date; to: Date } | undefined;
